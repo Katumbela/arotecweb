af5baf7747ff3543b1ab684f3d3186611f3ecfbb
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2023-04-20 13:08:21 +0100

    Update sobre.jsx

--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -17,8 +17,6 @@ import siao from '../imgs/siao2.jpeg'
 import goreth from '../imgs/goret.jpeg'
 import BannerPreto from '../components/banner_preto';
 import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
 import { NavLink } from 'react-router-dom';
 
 const Sobre = ({cart}) => {
