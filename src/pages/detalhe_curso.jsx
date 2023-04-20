c0bc5c443d38aaf267834b32e22b5e987c4fb389
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2023-04-20 13:07:26 +0100

    Update detalhe_curso.jsx

--- src/pages/detalhe_curso.jsx
+++ src/pages/detalhe_curso.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import '../css/destaque.css'
 import { NavLink, useParams } from 'react-router-dom';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import BannerPreto from '../components/banner_preto';
