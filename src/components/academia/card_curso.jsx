8ffc264f3dc28ad3d6f59f2b0eedcb427f84ded7
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2023-05-05 20:04:54 +0100

    s

--- src/components/academia/card_curso.jsx
+++ src/components/academia/card_curso.jsx
@@ -9,7 +9,7 @@ const CardCurso = ({ c }) => {
     const { id, curso, preco, duracao, lastUpdate, img } = c;
 
     return (
-                <NavLink className="item mx-auto navlink col-12 col-sm-6 col-md-4 col-lg-3" to={"/academia/curso/arotec/" + id}>
+                <NavLink className="item navlink col-6 col-sm-6 col-md-4 col-lg-3" to={"/academia/curso/arotec/" + id}>
                     <div className="">
                         <div className="card_cursos card-hover">
                             <div className="card-headerr">
