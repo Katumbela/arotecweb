6e36dff18aaf1e1d1fb627c9221bb830c21a299a
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2023-05-11 14:18:30 +0100

    Update politicas.jsx

--- src/en/pages/politicas.jsx
+++ src/en/pages/politicas.jsx
@@ -13,56 +13,52 @@ const Politicas = ({emaill, nomee, cart}) => {
       
       < Header  nomee={nomee} emaill={emaill} cart={cart} />
 <BannerPreto>
-  Políticas de Privacidade
+Privacy Policy
 </BannerPreto>
     <div className="container">
       <br />
       <div class="container">
         
-      <h2>Política de privacidade</h2><br />
+      <h2>Privacy Policy</h2><br />
 
 <p>
-O site <a href="http://www.arotec.ao">www.arotec.ao</a> é de propriedade da empresa AROTEC SU, que é a controladora de seus dados pessoais.
+The website <a href="http://www.arotec.ao">www.arotec.ao</a> is owned by the company AROTEC SU, which is the controller of your personal data.
+</p>
+<p>
+We have adopted this Privacy Policy, which determines how we are processing the information collected by the website www.arotec.ao and also explains why we need to collect personal data about you. Therefore, you must read this Privacy Policy before using the website www.arotec.ao.
 </p>
 
 <p>
-Nós adotamos esta Política de Privacidade, que determina como nós estamos processando as informações coletadas pelo site www.arotec.ao e também explica por quais razões nós precisamos coletar dados pessoais sobre você. Portanto, você deve ler esta Política de Privacidade antes de usar o site www.arotec.ao.
-</p>
-
-<p>
-Nós cuidamos dos seus dados pessoais e assumimos a responsabilidade de garantir a confidencialidade e segurança de suas informações pessoais.
+We take care of your personal data and assume responsibility for ensuring the confidentiality and security of your personal information.
 </p>
 
 <p>
 
-Estes são as informações pessoais que coletamos:
-Quando você visita o site www.arotec.ao, nós automaticamente coletamos certas informações sobre seu dispositivo, incluindo informações sobre seu navegador, endereço IP, fuso horário e alguns dos cookies instalados no seu sispositivo. Além disso, quando você navega pelo Site, nós coletamos informações sobre as páginas individuais ou produtos que você visualiza, sobre quais sites ou termos de busca redirecionaram você para nosso Site, e sobre como você interage com o Site. Nós nos referimos a essas informações coletadas automaticamente como "Informações sobre o dispositivo". Além disso, nós podemos coletar dados pessoais que você fornecer (incluindo, mas não limitado a: Nome, Sobrenome, Endereço, informações de pagamento, etc) durante o processo de registro para poder cumprir o acordo.
-
+These are the personal information we collect:
+When you visit the www.arotec.ao website, we automatically collect certain information about your device, including information about your browser, IP address, time zone and some of the cookies installed on your device. In addition, when you browse the Site, we collect information about the individual pages or products you view, which websites or search terms redirected you to our Site, and how you interact with the Site. We refer to this automatically collected information as "Device Information". In addition, we may collect personal data that you provide (including, but not limited to: First Name, Last Name, Address, payment information, etc.) during the registration process in order to fulfill the Agreement.
 </p>
-
-<b>Por que fazemos o processamento dos seus dados?</b><br /><br />
+<b>Why do we process your data?</b><br /><br />
 
 <p>
-Nossa maior prioridade é a segurança dos dados pessoais dos usuários e, portanto, nós podemos processar apenas dados mínimos, apenas enquanto for absolutamente necessário para a manutenção do site. Informações coletadas automaticamente são usadas para identificar possíveis casos de abuso e estabelecer dados estatísticos sobre o uso do site. Esses dados estatísticos não pe agregada de outras formas que permitam a identificação de usuários específicos do sistema.
+Our highest priority is the security of users' personal data and therefore we can process only minimal data, only as long as absolutely necessary for the maintenance of the site. Information collected automatically is used to identify possible cases of abuse and establish statistical data on the use of the website. This statistical data is not aggregated in other ways that allow the identification of specific users of the system.
 <br />
 <br />
-Você pode visitar o site sem nos contar sobre quem você é ou relevar qualquer informação que possa ser usada por outra pessoa para identificar você individualmente. Se, apesar disso, você quiser utilizar algum dos recursos do site, ou quiser receber nossa newsletter, ou quiser conceder outros detalhes através do preencimento e envio de formulários, você poderá fornecer dados pessoais para nós, como seu email, nome, sobrenome, cidade de residência, organização e número de telefone. Você pode escolher não fornecer dados pessoais para nós, mas, dessa forma, talvez você não consiga usar alguns dos resursos do site. Por exemplo, você não vai conseguir receber nossa Newsletter ou entrar em contato conosco diretamente pelo nosso site. Usuários que não tenham certeza sobre quais informações pessoais são obrigatórias são convidados a entrarem em contato conosco pelo e-mail arotec.info@gmail.com.
+You can visit the site without telling us who you are or revealing any information that could be used by someone else to uniquely identify you. If, despite this, you want to use any of the features of the site, or want to receive our newsletter, or want to provide other details by filling in and sending forms, you can provide personal data to us, such as your email, first name, last name, city of residence, organization and telephone number. You can choose not to provide us with personal data, but as a result, you may not be able to use some of the features on the site. For example, you will not be able to receive our Newsletter or contact us directly through our website. Users who are unsure which personal information is required are invited to contact us at arotec.info@gmail.com.
 
-</p>
-<b>Seus direitos:</b><br />
+</p><b>Your rights:</b><br />
 
-<p>Se você morar na Europa, estes são os direitos garantidos quando aos seus dados pessoais:
+<p>If you live in Europe, these are the rights guaranteed regarding your personal data:
 </p>
 
 
 <ul>
-  <li>O direito de ser informado.</li>
-  <li>O direito ao acesso.</li>
-  <li>O direito à retificação.</li>
-  <li>O direito de deletar.</li>
-  <li>O direito de restringir o processamento.</li>
-  <li>O direito da portabilidade de dados.</li>
-  <li>O direito à objeção.</li>
+  <li>The right to be informed.</li>
+  <li>The right to access.</li>
+  <li>The right to rectification.</li>
+  <li>The right to delete.</li>
+  <li>The right to restrict processing.</li>
+  <li>The right to data portability.</li>
+  <li>The right to object.</li>
 </ul>
 
 
@@ -71,46 +67,43 @@ Você pode visitar o site sem nos contar sobre quem você é ou relevar qualquer
 
 
 
-
 <p>
 
-Direitos em relação a tomadas de decisão automaticas e à perfilagem.
-Se você quiser exercitar esses direitos, por favor entre em contato conosco usando os dados de contato abaixo.
+Rights regarding automatic decision-making and profiling.
+If you would like to exercise these rights, please contact us using the contact details below.
 
 </p>
 
 <p>
 
-Adicionalmente, se você reside na Europa, nós afirmamos que estamos processando suas informações com a finalidade de cumprir contratos que possamos ter firmado com você (por exemplo, se você fizer uma compra no nosso Site), ou para excercer os interesses legítimos da nossa empresa listados acima. Além disso, por favor saiba que suas informações poder ser transferidas para fora da Europa, incluindo para o Canadá e os Estados Unidos da América.
+Additionally, if you reside in Europe, we state that we are processing your information for the purpose of fulfilling contracts we may have entered into with you (for example, if you make a purchase on our Site), or to pursue our company's legitimate interests. listed above. Also, please be aware that your information may be transferred outside of Europe, including Canada and the United States of America.
 
 </p>
 
 <p>
   <p>
-
-<b>Links para outros sites:</b><br />
-Nosso site pode conter links para outros sites que não são controlados por nós e/ou não são de nossa propriedade. Por favor esteja ciente de que nós não somos responsáveis pelas políticas de privacidade de tais sites e organizações terceiras. Nós incentivamos você a estar ciente de quando sair do nosso site, e também incentivamos você a ler a política de privacidade de cada um dos sites que podem coletar suas informações pessoais.
+  <b>Links to other sites:</b><br />
+Our website may contain links to other websites that are not controlled by us and/or are not owned by us. Please be aware that we are not responsible for the privacy policies of such third party websites and organizations. We encourage you to be aware when you leave our website, and we also encourage you to read the privacy policy of each of the websites that may collect your personal information.
 
   </p>
 </p>
 
 <p>
 
-<b>Segurança das informações:</b><br />
-Nós garantimos que as informações que você fornece estão em servidores e computadores armazenados em ambientes seguros e controlados, protegidos de acessos, usos e divulgações não-autorizadas. Nós matemos medidas de segurança administrativas, técnicas e físicas razoáveis, com finalidade de proteger os dados pessoais sob nossa custódia de de acessos, usos, modificações e divulgações não-autorizadas. Apesar disso, nenhuma transmissão de dados pela Internet ou por sistemas sem fio pode ser garantida.
+<b>Information security:</b><br />
+We guarantee that the information you provide is on servers and computers stored in secure and controlled environments, protected from unauthorized access, use and disclosure. We maintain reasonable administrative, technical and physical security measures in order to protect the personal data in our custody from unauthorized access, use, modification and disclosure. Despite this, no data transmission over the Internet or wireless systems can be guaranteed.
 
 </p>
 
 <p>
-
-<b>Declaração legal:</b><br />
-Nós vamos divulgar qualquer informação que coletarmos, usarmos ou recebermos caso tal divulgação seja solicitada ou permitida por lei, de forma a cumprir intimações ou processos judiciais similares, e também quando considerarmos em boa fé que a divulgação é necessária para a proteção de nossos direitos, para a proteção da segurança de outros, para investigações de fraude ou para responder a uma solicitação do governo.
+<b>Legal statement:</b><br />
+We will disclose any information we collect, use or receive if such disclosure is required or permitted by law, in order to comply with subpoenas or similar legal proceedings, and also when we believe in good faith that disclosure is necessary to protect our rights, for the protection of the safety of others, for fraud investigations or to respond to a government request.
 
 </p>
 <p>
 
-<b>Informações de contato:</b><br />
-Se você quiser entrar em contato conosco para saber mais sobre esta Política de Privacidade, ou quiser acessar quaisquer informações relativas aos seus direitos individuais e às suas Informações Pessoais, você poderá enviar um e-mail para o endereço arotec.info@gmail.com.
+<b>Contact information:</b><br />
+If you would like to contact us to learn more about this Privacy Policy, or would like to access any information regarding your individual rights and your Personal Information, you may send an email to arotec.info@gmail.com.
     
 </p>
 
