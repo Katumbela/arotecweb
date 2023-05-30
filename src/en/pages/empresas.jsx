22b57bfefd5c17d03f246f59782e3af3d52d8e97
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2023-05-30 17:37:58 +0100

    Update empresas.jsx

--- src/en/pages/empresas.jsx
+++ src/en/pages/empresas.jsx
@@ -39,7 +39,7 @@ const Empresas = ({nomee, emaill, cart}) => {
           </div>
         </div>
         <div className="col-12 col-sm-5 col-xl-6 col-lg-5 p-2">
-          <img src={aro} className='w-100' alt="" />
+          <img src={aro} className='' style={{height:'20em'}} alt="" />
           </div>
       </div>
       <br />
