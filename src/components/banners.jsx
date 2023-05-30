74f8540182455da97fd074d4b5b52c2840359dc9
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2023-05-30 14:24:47 +0100

    Update banners.jsx

--- src/components/banners.jsx
+++ src/components/banners.jsx
@@ -5,7 +5,7 @@ import banner_empresa from '../imgs/img1.png'
 
 function Banners() {
   return (
-    <Carousel className=''>
+    <Carousel className='car'>
       <Carousel.Item className="carousel px-5 b_code py-auto bg-c">
        
        <div className="row  px-4">
